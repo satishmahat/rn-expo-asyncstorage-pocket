aa74a4c5dd294c136066c8dabeeb99ba18426f19
satishmahat <mahatsatish@gmail.com>
2025-10-12 08:21:59 +0545

    Add react-native-gesture-handler for swipeable actions in ExpenseItemCard; wrap App component in GestureHandlerRootView for gesture handling support.

--- App.js
+++ App.js
@@ -3,15 +3,17 @@ import { StyleSheet, Text, View } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 import  {ExpenseProvider} from './src/context/ExpenseContext';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
   return (
+    <GestureHandlerRootView>
     <ExpenseProvider>
       <NavigationContainer>
         <AppNavigator />
       </NavigationContainer>
     </ExpenseProvider>
-    
+    </GestureHandlerRootView>
   );
 }
 
--- src/components/ExpenseItemCard.js
+++ src/components/ExpenseItemCard.js
@@ -1,11 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View} from 'react-native'
 import React from 'react'
 import tw from 'twrnc'
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+import { Ionicons } from '@expo/vector-icons';
 
 const ExpenseItemCard = ({item}) => {
-
+    const LeftSwipeActions = () => {
+    return (
+        <View style={tw`w-20 bg-blue-500 justify-center items-center rounded-2xl ml-5 -mr-3 mb-3`}>
+            <View style={tw`w-12 h-12 bg-white rounded-full items-center justify-center`}>
+                <Ionicons name="create-outline" size={24} color="#3B82F6" />
+            </View>
+        </View>
+    )
+    }
+    const rightSwipeActions = () => {
+    return (
+        <View style={tw`w-20 bg-red-500 justify-center items-center rounded-2xl mr-5 -ml-3 mb-3`}>
+            <View style={tw`w-12 h-12 bg-white rounded-full items-center justify-center`}>
+                <Ionicons name="trash-outline" size={24} color="#EF4444" />
+            </View>
+        </View>
+    )
+    }
   return (
-    <View style={tw`bg-white rounded-2xl p-4 mx-5 mb-3 flex-row items-center justify-between shadow-sm`}>
+    <Swipeable
+        renderLeftActions={LeftSwipeActions}
+        renderRightActions={rightSwipeActions}
+        leftThreshold={80}
+        rightThreshold={80}
+        friction={2}
+    >
+    <View style={[tw`bg-white rounded-2xl p-4 mx-5 mb-3 flex-row items-center justify-between shadow-sm`, {borderLeftWidth: 0.5, borderLeftColor: '#3B82F6', borderRightWidth: 0.5, borderRightColor: '#EF4444'}]}>
         <View style={tw`flex-row items-center flex-1 mr-3`}>
             <View style={tw`w-12 h-12 rounded-xl bg-gray-100 items-center justify-center mr-4`}>
                 <Text>{item.icon}</Text>
@@ -23,6 +49,7 @@ const ExpenseItemCard = ({item}) => {
             <Text style={tw`text-xs text-gray-500 mt-1`}>{item.date}</Text>
         </View>
     </View>
+    </Swipeable>
   )
 }
 
